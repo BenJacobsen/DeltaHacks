f90f3b90d78e47f71e20ac41f5f8832b28b28fb7
Ben Jacobsen <bbjacobs@uwaterloo.ca>
2020-01-26 07:56:54 -0500

    cmd script for making server and clients

---
+++ init.ps1
@@ -0,0 +1,3 @@
+ invoke-expression 'cmd /c start powershell -Command { py C:\Github\DeltaHacks\sockets.py }'
+ invoke-expression 'cmd /c start powershell -Command { py C:\Github\DeltaHacks\client.py }'
+ invoke-expression 'cmd /c start powershell -Command { py C:\Github\DeltaHacks\client.py }'
